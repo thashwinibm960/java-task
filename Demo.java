4c06fc3f384a3e99e13ec3e733cb2fe12955e359
Thashwini BM <thashwinibm085@gmail.com>
2024-10-07 15:00:33 +0530

    add project

---
+++ Demo.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author thash
+ */
+public class Demo {
+   
+
+public static void main(String[] args){
+
+byte colorsInRainbow = 7;
+byte lettersOfVowels = 5;
+byte freezingPoint = 0;
+byte boilingPoint = 100;
+byte numberOfWheelsInCar = 4;
+byte roomTemperature = 25;
+byte daysInMonth = 30;
+byte numberOfRoomsInHouse = 5;
+short yearOfBirth = 2002;
+short phoneBatteryLevel = 50;
+short pagesInBook = 350;
+short examTotalScore = 95;
+short numberOfEmployees = 150;
+short graduationYear = 2024;
+short numberOfStudents = 73;
+short priceOfMovieTicket = 200;
+System.out.println(colorsInRainbow);
+colorsInRainbow = 6;
+System.out.println(colorsInRainbow );
+System.out.println(lettersOfVowels );
+System.out.println(freezingPoint );
+System.out.println(boilingPoint );
+System.out.println(numberOfWheelsInCar );
+System.out.println(roomTemperature );
+System.out.println(daysInMonth );
+System.out.println(numberOfRoomsInHouse );
+System.out.println(yearOfBirth );
+System.out.println(phoneBatteryLevel );
+System.out.println(pagesInBook );
+System.out.println(examTotalScore );
+System.out.println(numberOfEmployees );
+System.out.println(graduationYear );
+System.out.println(numberOfStudents );
+System.out.println(priceOfMovieTicket );
+}
+}
+    
+
