94eab024d13e75c60d97116153e231228233aa1a
Thashwini BM <thashwinibm085@gmail.com>
2024-10-10 07:13:40 +0530

    add project

---
+++ method_overloading.java
@@ -0,0 +1,23 @@
+ 
+
+
+public class Text {
+    
+
+
+
+
+ public static void main(String[] args) {
+
+      String input = "thas" ;                       
+     getinfo(input);
+ }
+     public static void getinfo(String name)
+      {
+     	System.out.println("name is:" + name);
+     	
+     	
+     }
+ 	 
+	
+}
