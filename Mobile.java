9f09720389b7981cb1e8c70a0c321d8bdc2180f9
thashwinibm960 <thashwinibm960@gmail.com>
2024-10-15 08:33:18 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,28 @@
+public class Mobile{
+ 
+
+    String brand;
+    String model;
+    int batteryPercentage;
+    boolean isOn;
+
+public static void getBrand()
+{
+
+}
+public static void getModel()
+{
+
+}
+public static void getBatteryPercentage()
+{
+
+}
+public static void isOn()
+{
+
+}
+}
+
+
+    
---
+++ mobileInfo.java
@@ -0,0 +1,168 @@
+public class mobileInfo {
+
+    String brand;
+    String model;
+    int batteryPercentage;
+    boolean isOn;
+
+public static void main (String [] args)
+{
+	Mobile mobile = new Mobile();
+	
+	mobile.brand ="redmi";
+    mobile.model ="redmi note 12";
+    mobile.batteryPercentage=4000;
+    mobile.isOn=true ;
+
+	
+	Mobile mobile1 = new Mobile();
+	
+	mobile1.brand ="samsung";
+    mobile1.model ="galaxy S24 ultra";
+    mobile1.batteryPercentage=5000;
+    mobile1.isOn=false ;
+
+	
+	Mobile mobile2 = new Mobile();
+	
+	mobile2.brand ="Apple";
+    mobile2.model ="iphone 15";
+    mobile2.batteryPercentage=5000;
+    mobile2.isOn=true ;
+	
+	Mobile mobile3 = new Mobile();
+	
+	mobile3.brand ="redmi";
+    mobile3.model ="redmi note 9 pro";
+    mobile3.batteryPercentage=4000;
+    mobile3.isOn=false;
+
+
+	Mobile mobile4 = new Mobile();
+	
+	mobile4.brand ="samsung";
+    mobile4.model ="galaxy S23 ultra";
+    mobile4.batteryPercentage=4000;
+    mobile4.isOn=true ;
+	
+	Mobile mobile5 = new Mobile();
+	
+	mobile5.brand ="redmi";
+    mobile5.model ="redmi note 10";
+    mobile5.batteryPercentage=4000;
+    mobile5.isOn=true ;
+
+	
+	Mobile mobile6 = new Mobile();
+	
+	mobile6.brand ="Apple";
+    mobile6.model ="iphone 15 pro max";
+    mobile6.batteryPercentage=5000;
+    mobile6.isOn=false ;
+
+
+
+	Mobile mobile7 = new Mobile();
+	
+	mobile7.brand ="xiaomi";
+    mobile7.model ="redmi note 12 pro";
+    mobile7.batteryPercentage=4000;
+    mobile7.isOn=true ;
+
+	
+	Mobile mobile8 = new Mobile();
+	
+	mobile8.brand ="oneplus";
+    mobile8.model ="oneplus 11";
+    mobile8.batteryPercentage=5000;
+    mobile8.isOn=false ;
+
+	
+	Mobile mobile9 = new Mobile();
+	
+	mobile9.brand ="google";
+    mobile9.model ="pipx";
+    mobile9.batteryPercentage=4000;
+    mobile9.isOn=true ;
+
+
+
+
+
+
+
+
+
+    System.out.println("-----------------------------------");
+	System.out.println("mobile brand is :"+mobile.brand);
+	System.out.println("model :"+mobile.model);
+	System.out.println("batteryPercentage :"+mobile.batteryPercentage);
+	System.out.println("isOn :"+mobile.isOn);
+
+
+	System.out.println("-----------------------------------");
+	System.out.println("mobile brand is :"+mobile1.brand);
+	System.out.println("model :"+mobile1.model);
+	System.out.println("batteryPercentage :"+mobile1.batteryPercentage);
+	System.out.println("isOn :"+mobile1.isOn);
+
+	System.out.println("-----------------------------------");
+	System.out.println("mobile brand is :"+mobile2.brand);
+	System.out.println("model :"+mobile2.model);
+	System.out.println("batteryPercentage :"+mobile2.batteryPercentage);
+	System.out.println("isOn :"+mobile2.isOn);
+
+	System.out.println("-----------------------------------");
+	System.out.println("mobile brand is :"+mobile3.brand);
+	System.out.println("model :"+mobile3.model);
+	System.out.println("batteryPercentage :"+mobile3.batteryPercentage);
+	System.out.println("isOn :"+mobile3.isOn);
+
+
+
+
+	System.out.println("-----------------------------------");
+	System.out.println("mobile brand is :"+mobile4.brand);
+	System.out.println("model :"+mobile4.model);
+	System.out.println("batteryPercentage :"+mobile4.batteryPercentage);
+	System.out.println("isOn :"+mobile4.isOn);
+
+	System.out.println("-----------------------------------");
+	System.out.println("mobile brand is :"+mobile5.brand);
+	System.out.println("model :"+mobile5.model);
+	System.out.println("batteryPercentage :"+mobile5.batteryPercentage);
+	System.out.println("isOn :"+mobile5.isOn);
+
+	System.out.println("-----------------------------------");
+	System.out.println("mobile brand is :"+mobile6.brand);
+	System.out.println("model :"+mobile6.model);
+	System.out.println("batteryPercentage :"+mobile6.batteryPercentage);
+	System.out.println("isOn :"+mobile6.isOn);;
+
+	System.out.println("-----------------------------------");
+	System.out.println("mobile brand is :"+mobile7.brand);
+	System.out.println("model :"+mobile7.model);
+	System.out.println("batteryPercentage :"+mobile7.batteryPercentage);
+	System.out.println("isOn :"+mobile7.isOn);
+
+	System.out.println("-----------------------------------");
+	System.out.println("mobile brand is :"+mobile8.brand);
+	System.out.println("model :"+mobile8.model);
+	System.out.println("batteryPercentage :"+mobile8.batteryPercentage);
+	System.out.println("isOn :"+mobile8.isOn);
+
+	System.out.println("-----------------------------------");
+	System.out.println("mobile brand is :"+mobile9.brand);
+	System.out.println("model :"+mobile9.model);
+	System.out.println("batteryPercentage :"+mobile9.batteryPercentage);
+	System.out.println("isOn :"+mobile9.isOn);
+
+	System.out.println("-----------------------------------");
+
+
+
+
+
+}
+	
+}
