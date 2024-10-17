f08693bf32d880a122ff20ff7f24c191f6d2d35e
thashwinibm960 <thashwinibm960@gmail.com>
2024-10-17 18:27:20 +0530

    Add files via upload

--- MachineDetails.java
+++ MachineDetails.java
@@ -11,7 +11,7 @@ public class MachineDetails {
         WashMachine washmachine4 = new WashMachine("IFB", "medium", "1000 RPM", 7);
         WashMachine washmachine5 = new WashMachine("HAIER", "large", "1800 RPM", 12);
         WashMachine washmachine6 = new WashMachine("LG", "Small", "800 RPM", 6);
-        WashMachine washmachine7 = new WashMachine("SAMSUNG", "medium", "1000 RPM", 7);
+        WashMachine washmachine7 = new WashMachine("SAMSUNG", "medium", "5000 RPM", 8);
         WashMachine washmachine8 = new WashMachine("HAIER", "large", "1800 RPM", 12);
 
        
@@ -25,12 +25,38 @@ public class MachineDetails {
         SaveDetails(washmachine7);
         SaveDetails(washmachine8);
 
+        System.out.println("......machine details ........");
+
 
         for (int i = 0; i < machinedata.length; i++) {
             if (machinedata[i] != null) {
                 machinedata[i].WashMachineDetails();
+                
             }
         }
+         
+          System.out.println("......find machine by name........");
+         WashMachine foundMachine = findByMachineName("BOSCH");
+         WashMachine foundMachine2 = findByMachineName("WHIRLPOOL");
+         if (foundMachine != null) {
+         	foundMachine.WashMachineDetails(); 
+         	foundMachine2.WashMachineDetails();
+         }
+
+        
+        System.out.println("......update machine by it speed and name........");
+
+        UpdateMachineBySpeed("SAMSUNG" ,"2000 RPM");
+         UpdateMachineBySpeed("IFB" ,"1500 RPM");
+
+        for (int i=0;i< machinedata.length ;i++ ) {
+        	if(machinedata[i] != null){
+        		machinedata[i].WashMachineDetails();
+        		
+        	}
+        	
+        }
+       
     }
 
     public static boolean SaveDetails(WashMachine washmachine) {
@@ -43,5 +69,41 @@ public class MachineDetails {
         }
         System.out.println("Space full");
         return false;
+    }
+
+    public static WashMachine findByMachineName(String brand_name){
+    	for (int i=0;i< machinedata.length ;i++ ) {
+    		if (machinedata[i]!= null) {
+    			if (machinedata[i].brand_name.equals(brand_name)) {
+    				machinedata[i].brand_name = brand_name;
+    				System.out.println("brand name find");
+    				return machinedata[i];
+    				
+    			}
+    			
+    		}
+    		
+    	}
+    	System.out.println("name not found");
+    	return null;
+    }
+
+
+    public static boolean UpdateMachineBySpeed(String brand_name  , String speed )
+    {
+    	for (int i =0;i < machinedata.length ;i++ ) {
+    		if(machinedata[i] != null){
+    			if(machinedata[i].brand_name.equals(brand_name)){
+    				machinedata[i].speed = speed;
+    				System.out.println("updated machine speed");
+    				return true;
+    			}
+
+    		}
+    		
+    	}
+    	System.out.println("not updated");
+    	return false;
+
     }
 }
